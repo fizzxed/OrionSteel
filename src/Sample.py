be13a1f8a67275debb5e877a431413df714e15f6
BuildTools <unconfigured@null.spigotmc.org>
2016-02-20 15:28:30 -0500

    Initial sounds + test code

--- src/Sample.py
+++ src/Sample.py
@@ -6,12 +6,12 @@
 # between Leap Motion and you, your company or other organization.             #
 ################################################################################
 
-import os, sys, inspect
+import os, sys, inspect, thread, time
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
-import Leap, sys, thread, time
+import Leap
 
 
 class SampleListener(Leap.Listener):
